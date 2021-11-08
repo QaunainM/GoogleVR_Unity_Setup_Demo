aecdbc831a3bcf51cc7a6c04942f7d7a11e34aea
Qaunain Meghjee <65816071+QaunainM@users.noreply.github.com>
2021-11-08 13:49:14 +0000

    GoogleVR for Unity Demo Scene created by Qaunain Meghjee
    GoogleVR Demo Scene created by Qaunain Meghjee
    
    Using Unity Engine Version:  2019.4.32f1
    
    Using Google VR Android Version: 2.0.0

---
+++ Assets/QM_Demo_Scene/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject cameraRig;
+    public float moveOffset = 50f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraRig = GameObject.Find("CameraRig"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+            //Debug.Log(hit.transform.name); 
+            if (hit.transform.tag == "Enemy") {
+               cameraRig.transform.position = transform.position + transform.forward * moveOffset * Time.deltaTime;
+            }
+        }
+        
+    }
+}
